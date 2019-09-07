d7fb2c9fe80a036f48b58b1b624a9def048e287f
EumHongin <zizon0wpxm@naver.com>
2019-09-07 16:28:53 +0900

    createuser

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,31 @@
 const functions = require('firebase-functions');
-const firebase = require('firebase')
-const express = require('express');
 
+const express = require('express');
+const firebase = require("firebase-admin");
+
+var serviceAccount = require("./workflow-9e1eb-firebase-adminsdk-6sht8-9081c58c39.json");
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://workflow-9e1eb.firebaseio.com"
+});
+const db = firebase.firestore();
+const auth = firebase.auth()
 const app = express();
-var firebaseConfig = {
- apiKey: "AIzaSyAI05vAIQyXxoInyjMljxe89QMnr2zAYAA",
- authDomain: "workflow-9e1eb.firebaseapp.com",
- databaseURL: "https://workflow-9e1eb.firebaseio.com",
- projectId: "workflow-9e1eb",
- storageBucket: "workflow-9e1eb.appspot.com",
- messagingSenderId: "244876828920",
- appId: "1:244876828920:web:1ff838236786cf1285db0d"
-};
-firebase.initializeApp(firebaseConfig)
+
 // const router = express.Router()
 
-app.get('/helloworld',(req,res,next) => {
- 
- firebase.firestore().collection('users').doc('cocosdsdco').set({
-  first : 'hellowlrld'
- }).then((docRef)=>{
-  res.json({
-   msg: docRef.id
-  })
- })
+app.post('/createUser',(req,res,next) => {
+    let loginObject = {
+        email : req.body.email,
+        password : req.body.password,
+        name : req.body.name
+    }
+    auth.createUserWithEmailAndPassword(loginObject.email,loginObject.password).then(() => {
+
+    })
+    db.collection('users').add(loginObject)
+
  
 })
 
